6cd3862ba918a79ebe345dff4a884dd6e5c4116e
yudha <yamashitataiki123@gmail.com>
2022-10-27 23:57:18 +0700

    Update BarUI.cs

--- Assets/Scripts/BarUI.cs
+++ Assets/Scripts/BarUI.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BarUI : MonoBehaviour
+namespace CapedHorse.BallBattle
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BarUI : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
+
